c544bda01e3d95046abd629a9a2815e0d09888f1
Jahin-Tazwra <tazwarjahin@gmail.com>
2024-11-13 16:52:59 +0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+
+import Home from './scereens/home';
+
+import '@/index.css';
+
+const paths = [
+  {
+    path: "/",
+    element: (
+      <Home />
+    )
+  }
+]
+
+const BrowserRouter = createBrowserRouter(paths);
+
+const App = () => {
+  return (
+    <MantineProvider>
+      <RouterProvider router={BrowserRouter} />
+    </MantineProvider>
+  )
+}
+
+export default App
